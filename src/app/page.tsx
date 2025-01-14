7d3c28d76fecc9f6e8591e8c5dded920e5071819
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2025-01-14 07:37:06 +0700

    init pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,123 @@
-import Image from "next/image";
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
 
-export default function Home() {
+import { Button } from '@/components/ui/button';
+import React, { useEffect } from 'react';
+import { toast, Toaster } from "sonner";
+
+interface IMoveSet {
+  used: boolean;
+  value: number;
+}
+
+const Home: React.FC = () => {
+  const [moveSets, setMoveSets] = React.useState<IMoveSet[]>([]);
+
+  const init = () => {
+    const saved = localStorage.getItem('dice-alternative');
+    if (saved) {
+      setMoveSets(JSON.parse(saved));
+      toast.success("Move sets loaded from local storage");
+    } else {
+      generateMoveSets();
+    }
+  }
+
+  const generateMoveSets = () => {
+    const newMoveSets: IMoveSet[] = Array.from({ length: 200 }, () => ({
+      used: false,
+      value: Math.floor(Math.random() * (6 - 1) + 1),
+    }));
+    setMoveSets(newMoveSets);
+    toast.success("New move sets generated");
+  }
+
+  const saveMoveSets = () => {
+    localStorage.setItem('dice-alternative', JSON.stringify(moveSets));
+    toast.success("Move sets saved to local storage");
+  }
+
+  const clearMoveSets = () => {
+    localStorage.removeItem('dice-alternative');
+    generateMoveSets();
+    toast.success("Move sets cleared");
+  }
+
+  const useMoveSets = (id: number) => {
+    const lastUsed = moveSets.filter((moveSet) => moveSet.used).length
+    if (id !== lastUsed) {
+      toast.error(
+        "You can only use the next move set after your last move set"
+      );
+      return;
+    }
+
+    const newMoveSets = [...moveSets];
+    newMoveSets.forEach((moveSet: IMoveSet, index) => {
+      if (index === id) {
+        moveSet.used = true;
+      }
+    });
+    setMoveSets(newMoveSets);
+    saveMoveSets();
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="min-h-screen w-full bg-zinc-800 p-6">
+        <div className="container mx-auto w-full min-h-[300px] flex justify-center gap-4 flex-col my-6">
+          <h1 className="text-5xl text-zinc-100 leading-relaxed">
+            Dice Alternative
+          </h1>
+          <div className="max-w-[800px] mb-6">
+            <p className="text-lg text-zinc-300 leading-relaxed">
+              Generate pre defined move set value without rolling your dice.
+              Every time you reload the page, it will generate up to 200 move
+              set value. Click save to mark the seed and it will saved to your
+              local storage. So you wont lose the pattern. Next you can use the
+              value iteratively for move, attack, or any action on your game.
+              There is no luck because its already defined your move set. Click
+              the tile to mark as used.
+            </p>
+          </div>
+          <div className="flex items-center w-full gap-4">
+            <Button
+              onClick={saveMoveSets}
+              className="bg-zinc-200 text-zinc-900 hover:bg-zinc-300"
+            >
+              Save Move Set
+            </Button>
+            <Button
+              onClick={clearMoveSets}
+              className="bg-zinc-200 text-zinc-900 hover:bg-zinc-300"
+            >
+              Clear Move Set
+            </Button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="container mx-auto grid grid-cols-6 md:grid-cols-10 gap-2 mb-8">
+          {moveSets.map((moveSet: IMoveSet, index: number) => (
+            <Button
+              key={index}
+              className={`w-full h-full bg-zinc-700 border border-zinc-800 flex justify-center items-center text-zinc-100 ${
+                moveSet.used ? "opacity-50" : "cursor-pointer"
+              }`}
+              onClick={() => useMoveSets(index)}
+              disabled={moveSet.used}
+            >
+              <span>{moveSet.value}</span>
+            </Button>
+          ))}
+        </div>
+      </div>
+      <Toaster />
+    </>
   );
 }
+
+export default Home;
